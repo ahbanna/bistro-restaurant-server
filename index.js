c7b6b04060f0c6469fe242e7671f77804a3deb2f
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-29 21:45:32 +0600

    connect with mongodb

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+// Mongodb starts
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k7baavr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // collection starts
+    const menuCollection = client.db("bistro").collection("menu");
+    const reviewCollection = client.db("bistro").collection("reviews");
+    // collection end
+
+    // menu READ starts
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    // menu READ end
+
+    // review READ starts
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+    // review READ end
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Mongodb end
+
+app.get("/", (req, res) => {
+  res.send("Bistro restaurant server is running");
+});
+app.listen(port, () => {
+  console.log(`Bistro restaurant is running on port:${port}`);
+});
