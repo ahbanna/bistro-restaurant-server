d47f251fd0263392520c55fc470f4249e87571bb
Al Hasanul Banna <tutul5800@gmail.com>
2023-06-03 18:26:06 +0600

    update user role

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 // Mongodb starts
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k7baavr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +29,7 @@ async function run() {
     const menuCollection = client.db("bistro").collection("menu");
     const reviewCollection = client.db("bistro").collection("reviews");
     const cartCollection = client.db("bistro").collection("carts");
+    const userCollection = client.db("bistro").collection("users");
     // collection end
 
     // menu READ starts
@@ -65,6 +66,53 @@ async function run() {
       res.send(result);
     });
     // carts READ end
+
+    // carts DELETE starts
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+    // carts DELETE end
+
+    // user CREATE api to receive data from client side starts
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      // for google starts
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      console.log("Existing user:", existingUser);
+      if (existingUser) {
+        return res.send({ message: "user already exists " });
+      }
+      // for google end
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    // user CREATE api to receive data from client side end
+
+    // user READ starts
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    // user READ end
+
+    // make UPDATE user as admin starts
+    app.patch("users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // make UPDATE user as admin end
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
